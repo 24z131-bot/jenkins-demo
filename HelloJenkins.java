67bf9c58e2281d2e8b806da4e425064caf7a243c
CHIDDESH R <24z131@psgitech.ac.in>
2026-09-25 09:25:01 +0530

    Add HelloJenkins class with main method

---
+++ HelloJenkins.java
@@ -0,0 +1,6 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+        System.out.println("This Java program is running through Jenkins.");
+    }
+}
